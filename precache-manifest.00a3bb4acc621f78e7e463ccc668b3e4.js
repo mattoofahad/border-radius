77171b959980abf9b13a0efb43f16f802c1b9f28
simooooone <s.foschi@protonmail.com>
2020-09-03 17:12:55 +0200

    Updates

---
+++ precache-manifest.00a3bb4acc621f78e7e463ccc668b3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bea655db59ed0ad7b3a77e2e7609e68",
+    "url": "/border-radius/index.html"
+  },
+  {
+    "revision": "66f5d08e2841db78437b",
+    "url": "/border-radius/static/css/main.210c7c87.chunk.css"
+  },
+  {
+    "revision": "7667e1db64b406bf25b4",
+    "url": "/border-radius/static/js/2.ce49d928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/border-radius/static/js/2.ce49d928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f5d08e2841db78437b",
+    "url": "/border-radius/static/js/main.eef7ad02.chunk.js"
+  },
+  {
+    "revision": "30dc68bfda4c183eab29",
+    "url": "/border-radius/static/js/runtime-main.5e6094ac.js"
+  }
+]);
